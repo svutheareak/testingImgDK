51931bbc346ea79023092df15ca903a332e434a4
Sam Vutheareak <sam.vutheareak@FA-993-601-1082.local>
2025-05-19 15:58:48 +0700

    update serverJS

--- simple-web-ap/server.js
+++ simple-web-ap/server.js
@@ -1,16 +1,50 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// // Serve static files from the 'public' directory
+// app.use(express.static(path.join(__dirname, '../public')));
+
+// // Optional: Define API routes
+// app.get('/users/get', (req, res) => {
+//   res.json({ message: 'Hello from Fargate backend!' });
+// });
+
+// // Optional: Define API routes
+// app.get('/users/nana', (req, res) => {
+//   res.json({ message: 'Hello from Fargate nana!' });
+// });
+
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
 const express = require('express');
 const path = require('path');
 const app = express();
+
+// Define the port to listen on
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Optional: Define API routes
+// Define a simple route
 app.get('/users/get', (req, res) => {
   res.json({ message: 'Hello from Fargate backend!' });
 });
 
-app.listen(PORT, () => {
+
+
+// Optional: Define API routes
+app.get('/users/nana', (req, res) => {
+  res.json({ message: 'Hello from Fargate nana!' });
+});
+
+// Start the server and listen on all network interfaces
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
